8327ccd9f340fa6a578923f415170ac1e138cf24
SirEthic <164008988+SirEthic@users.noreply.github.com>
2026-09-02 01:10:50 +0530

    feat: Add Shadow Wave gesture via Ambient Light Sensor for Gig Mode

--- StemSync/lib/main.dart
+++ StemSync/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wakelock_plus/wakelock_plus.dart';
+import 'package:environment_sensors/environment_sensors.dart';
 import 'proxy_client.dart';
 import 'dart:ui';
 import 'package:flutter/services.dart';
@@ -128,6 +129,10 @@ class _MixerScreenState extends State<MixerScreen> with SingleTickerProviderStat
   final GlobalKey<CloudLibraryTabState> _cloudTabKey = GlobalKey<CloudLibraryTabState>();
   bool _isLoading = false;
   bool _isGigMode = false;
+  final environmentSensors = EnvironmentSensors();
+  StreamSubscription<double>? _lightSubscription;
+  double _baselineLight = 0.0;
+  DateTime _lastWave = DateTime.now();
   DateTime? _lastBackPressTime;
   
   List<TrackData> _tracks = [];
